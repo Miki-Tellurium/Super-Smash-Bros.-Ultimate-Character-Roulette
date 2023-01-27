8bb833e488d4941831ace0846456e2b1d97ffed8
Miki-Tellurium <michelegamer14@gmail.com>
2023-01-27 11:11:16 +0100

    Added color converter class

---
+++ src/com/mikitellurium/SmashRouletteFX/ColorConverter.java
@@ -0,0 +1,22 @@
+package com.mikitellurium.SmashRouletteFX;
+
+import javafx.scene.paint.Color;
+
+import java.util.Arrays;
+
+public class ColorConverter {
+
+    /* Convert rgb color value to a printable string */
+    public static String colorToString(double red, double green, double blue) {
+        return (int)red + "," + (int)green + "," + (int)blue;
+    }
+
+    /* Convert a printable string to a color and return it */
+    public static Color stringToColor(String colorString) {
+        var rgb = Arrays.stream(colorString.split(","))
+                .map(String::trim)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        return Color.rgb(rgb[0], rgb[1], rgb[2]);
+    }
+}
