ee8af46fb1815c0165b1b435cd5dadec6be4e4a2
Miki-Tellurium <michelegamer14@gmail.com>
2023-01-27 12:54:08 +0100

    Added Launcher class for JavaFX

---
+++ src/com/mikitellurium/SmashRoulette/Launcher.java
@@ -0,0 +1,7 @@
+package com.mikitellurium.SmashRoulette;
+/* This class allow running the application with JavaFX */
+public class Launcher {
+    public static void main(String[] args) {
+        SmashRoulette.main(args);
+    }
+}
