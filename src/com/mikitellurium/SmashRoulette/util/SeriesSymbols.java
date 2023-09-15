ebd9de92b8be74de5a47fd269cc056f94c2decd0
Miki_Tellurium <michelegamer14@gmail.com>
2023-09-15 11:59:26 +0200

    Added series symbols constants

---
+++ src/com/mikitellurium/SmashRoulette/util/SeriesSymbols.java
@@ -0,0 +1,46 @@
+package com.mikitellurium.SmashRoulette.util;
+
+public class SeriesSymbols {
+
+    public static final String ANIMAL_CROSSING = "AnimalCrossingSymbol";
+    public static final String ARMS = "ARMSSymbol";
+    public static final String BANJO_KAZOOIE = "BanjoKazooieSymbol";
+    public static final String BAYONETTA = "BayonettaSymbol";
+    public static final String CASTLEVANIA = "CastlevaniaSymbol";
+    public static final String DONKEY_KONG = "DKSymbol";
+    public static final String DRAGON_QUEST = "DragonQuestSymbol";
+    public static final String DUCK_HUNT = "DuckHuntSymbol";
+    public static final String EARTHBOUND = "EarthboundSymbol";
+    public static final String FATAL_FURY = "FatalFurySymbol";
+    public static final String FINAL_FANTASY = "FinalFantasySymbol";
+    public static final String FIRE_EMBLEM = "FireEmblemSymbol";
+    public static final String F_ZERO = "FZeroSymbol";
+    public static final String GAME_AND_WATCH = "Game&WatchSymbol";
+    public static final String GYROMITE = "GyromiteSymbol";
+    public static final String ICE_CLIMBERS = "IceClimberSymbol";
+    public static final String KID_ICARUS = "KidIcarusSymbol";
+    public static final String KINGDOM_HEARTS = "KingdomHeartsSymbol";
+    public static final String KIRBY = "KirbySymbol";
+    public static final String SUPER_MARIO = "MarioSymbol";
+    public static final String MEGA_MAN = "MegaManSymbol";
+    public static final String METAL_GEAR = "MetalGearSymbol";
+    public static final String METROID = "MetroidSymbol";
+    public static final String MINECRAFT = "MinecraftSymbol";
+    public static final String PAC_MAN = "PacManSymbol";
+    public static final String PERSONA = "PersonaSymbol";
+    public static final String PIKMIN = "PikminSymbol";
+    public static final String POKEMON = "PokemonSymbol";
+    public static final String PUNCH_OUT = "PunchOutSymbol";
+    public static final String SUPER_SMASH_BROS = "SmashBrosSymbol";
+    public static final String SONIC = "SonicSymbol";
+    public static final String SPLATOON = "SplatoonSymbol";
+    public static final String STAR_FOX = "StarFoxSymbol";
+    public static final String STREET_FIGHTER = "StreetFighterSymbol";
+    public static final String TEKKEN = "TekkenSymbol";
+    public static final String WARIO = "WarioSymbol";
+    public static final String WII_FIT = "WiiFitSymbol";
+    public static final String XENOBLADE_CHRONICLES = "XenobladeSymbol";
+    public static final String YOSHI = "YoshiSymbol";
+    public static final String ZELDA = "ZeldaSymbol";
+
+}
