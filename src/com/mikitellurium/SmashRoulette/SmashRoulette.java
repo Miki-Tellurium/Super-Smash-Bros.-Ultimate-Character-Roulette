832e7802af67d7f0e68a385218dffb36bfc41a06
Miki_Tellurium <michelegamer14@gmail.com>
2024-04-01 18:23:52 +0200

    Bump version

--- src/com/mikitellurium/SmashRoulette/SmashRoulette.java
+++ src/com/mikitellurium/SmashRoulette/SmashRoulette.java
@@ -22,7 +22,7 @@ import java.io.IOException;
 
 public class SmashRoulette extends Application {
 
-    public static final String VERSION = "2.1.0";
+    public static final String VERSION = "2.2.0";
 
     public static void main(String[] args) {
         launch(args);
